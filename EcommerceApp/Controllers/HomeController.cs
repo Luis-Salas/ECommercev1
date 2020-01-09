682b68707a3605dad624784b839cd5cd4cc3f41d
Luis Salas <salas.luis40@gmail.com>
2020-01-09 11:26:22 -0600

    adding migrations and register

--- EcommerceApp/Controllers/HomeController.cs
+++ EcommerceApp/Controllers/HomeController.cs
@@ -1,9 +1,25 @@
-    using Microsoft.AspNetCore.Mvc;
-    using EcommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+using EcommerceApp.Models;
     namespace EcommerceApp.Controllers 
     {
-        public class Home : Controller  
+        public class HomeController : Controller  
         {
+             private HomeContext dbContext;
+     
+            // here we can "inject" our context service into the constructor
+            public HomeController(HomeContext context)
+            {
+                dbContext = context;
+            }
             [HttpGet]      
             [Route("")]     
             public ViewResult Index()
@@ -16,5 +32,33 @@
             {
                 return View("ContentCreator");
             }
+        [HttpPost("Create")]
+        public IActionResult Create(User myUser)
+        {
+            System.Console.WriteLine("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee");
+            System.Console.WriteLine(myUser.FirstName);
+
+            if(ModelState.IsValid) 
+            {   
+                            System.Console.WriteLine("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee");
+
+                if(dbContext.Users.Any(u => u.Email == myUser.Email))
+                {   
+                    ModelState.AddModelError("Email", "Email already in use!");
+                    return View("Index");
+                } 
+                else
+                {   
+                    PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                    myUser.Password = Hasher.HashPassword(myUser, myUser.Password);
+                    dbContext.Users.Add(myUser);
+                    dbContext.SaveChanges();
+                    return RedirectToAction("Login");    
+                }
+                    
+            }
+            return View("Index");
+        }
+
         }
     }
---
+++ EcommerceApp/Migrations/20200109075932_initialMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApp.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    PageId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: false),
+                    about = table.Column<string>(nullable: false),
+                    UsersPageUserId = table.Column<int>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.PageId);
+                    table.ForeignKey(
+                        name: "FK_Pages_Users_UsersPageUserId",
+                        column: x => x.UsersPageUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pages_UsersPageUserId",
+                table: "Pages",
+                column: "UsersPageUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
